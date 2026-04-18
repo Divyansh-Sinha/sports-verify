34a8a373a22509aa543d3a70b1e00d3e8353798f
Divyansh-Sinha <divyansh8252230@gmail.com>
2026-04-18 15:22:37 +0530

    Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
     <App />
+    <SpeedInsights />
   </HashRouter>
 );
