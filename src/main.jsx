fc2b1c9f8b29de372105cac3ccb9b6964f7ac5a5
Divyansh-Sinha <divyansh8252230@gmail.com>
2026-04-18 15:24:22 +0530

    Fix vercel Speed Insights import

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { HashRouter } from 'react-router-dom';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById('root')).render(
   <HashRouter>
